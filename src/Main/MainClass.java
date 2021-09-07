cc51edcac73d50374b79c8ee82638f75bd0d4635
Paul <66725843+Paul-H18@users.noreply.github.com>
2021-09-07 16:17:55 +0200

    sout "Ses"

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -4,11 +4,17 @@ import OwnCollection.Stack;
 
 public class MainClass {
 
+
+
     public static void main(String[] args) {
         Stack s = new Stack(10);
         s.push("Hallo");
         System.out.println(s.top());
         s.pop();
         System.out.println(s.isEmpty());
+
+
+        //Test
+        System.out.println("Ses");
     }
 }
