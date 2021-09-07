fae765de1d448ce566cfa40a485d3cd96c13a2e4
Florian Oppermann <pixhd@famoushistory.de>
2021-09-07 09:58:22 +0200

    Initial commit

---
+++ src/Stack.java
@@ -0,0 +1,32 @@
+
+public class Stack {
+
+    private int index; //Indexposition des Aktuellen Elements
+    private int anzahlElemente;
+
+
+    public Stack() {
+
+    }
+
+    public Stack(int anzahl) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public void push(Object o) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public Object top() {
+        return null;
+    }
+
+}
