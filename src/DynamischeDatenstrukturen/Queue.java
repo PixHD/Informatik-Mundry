edfe8378fada3fb247db3b9ba7ee7d78ebf811ac
Florian Oppermann <pixhd@famoushistory.de>
2021-11-16 10:23:24 +0100

    addet Box.java and Queue.java

--- src/DynamischeDatenstrukturen/Queue.java
+++ src/DynamischeDatenstrukturen/Queue.java
@@ -2,4 +2,42 @@ package DynamischeDatenstrukturen;
 
 public class Queue {
 
+    private Box front;
+
+    public Queue(){
+        front =  null;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+
+    public void enqueue(Object e) {
+        if(isEmpty()) {
+            //Fall 1: Queue ist Leer
+            front = new Box(e, null);
+        }else {
+            //Fall 2: Queue enthält mind. 1 Element
+            Box tmp = front;
+            //While Schleife um ans Ende der Queue zu gelangen.
+            while(tmp.getNext() != null) {
+                tmp = tmp.getNext();
+            }
+            tmp.setNext(new Box(e, null));
+        }
+    }
+
+    public void dequeue() {
+        if(!isEmpty()) {
+            //Fall 1: Liste ist nicht Leer
+            front = front.getNext();
+        }else {
+            //Fall 2: Liste ist bereits leer
+           return;
+        }
+    }
+
+    public Object front() {
+        return front;
+    }
 }
