a1f17f1be81d19940eb80c146483365747bb3e69
Paul <66725843+Paul-H18@users.noreply.github.com>
2021-11-16 10:16:37 +0100

    Class Card added

---
+++ src/PaulCollection/DynamicQueue.java
@@ -0,0 +1,27 @@
+package PaulCollection;
+
+
+public class DynamicQueue {
+    private Box front;
+
+    public DynamicQueue() {
+        front = null;
+    }
+
+    public boolean isEmpty() {
+        if(front == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void enqueue(Object o) {
+
+    }
+
+
+    public void dequeue() {
+
+    }
+}
