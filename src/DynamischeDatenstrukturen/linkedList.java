434f99914f23eb0ab27fdd857b303abc198e4f8a
Florian Oppermann <pixhd@famoushistory.de>
2021-12-07 10:41:39 +0100

    Added linkedList.java

---
+++ src/DynamischeDatenstrukturen/linkedList.java
@@ -0,0 +1,98 @@
+package DynamischeDatenstrukturen;
+
+public class linkedList {
+
+    private Box it = null;
+    private Box first = null;
+    private Box last = null;
+
+
+    public linkedList(Object o) {
+        this.first = new Box(o);
+        this.last = first;
+        this.it = first;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public boolean hasAccess() {
+        return it != null;
+    }
+
+    public void next() {
+        first = first.getNext();
+    }
+
+    public void setObject(Object o) {
+        it.setItem(o);
+    }
+
+    public Box getObject() {
+        return it;
+    }
+
+    public void toFirst() {
+        first = it;
+    }
+
+    public void toLast() {
+        last = it;
+    }
+
+    public void insert(Object o) {
+        if(isEmpty()) { //Fall 1: Liste ist Leer
+            first = new Box(o);
+            last = first;
+        }else { //Fall 2: Liste hat Objekte
+            if(!hasAccess()) return;//Fall 3: Es gibt kein Akt. Objekt
+            Box tmp = first;
+            while(tmp.getNext() != it) {
+                tmp = tmp.getNext();
+            }
+            tmp.setNext(new Box(o, it));
+        }
+    }
+
+    public void remove() {
+        if(isEmpty()) return; //Fall 1: Liste ist Leer
+        if(hasAccess()) {
+            if(it.getNext() == null){
+                //Fall 3: Akt. Objet ist Letztes in der Liste
+                Box tmp = first;
+                while(tmp.getNext() != it) {
+                    tmp = tmp.getNext();
+                }
+                tmp.setNext(null);
+                last = tmp;
+                it = null;
+            }else {
+                //Fall 4: Es gibt ein Akt. Objekt, welches nicht das Letzte ist.
+                Box tmp = first;
+                Box afterIt = it.getNext();
+                while(tmp.getNext() != it) {
+                    tmp = tmp.getNext();
+                }
+                tmp.setNext(afterIt);
+                it = afterIt;
+            }
+        }else {
+            return; //Fall 2: Es gibt kein Akt. Objekt
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
